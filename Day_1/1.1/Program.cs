2b90065dc7af4d3f4dbf3a5dc463e8fb94ab6ba3
Aaron <asrose274@gmail.com>
2025-12-02 14:52:41 -0700

    more changes

---
+++ Day_1/1.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\rosea\\Repos\\AdventOfCode2025\\Day_1\\input.txt";
+
+        var lines = File.ReadAllLines(filePath);
+
+        int dial = 50;
+        int numOfZeros = 0;
+        int clicks;
+
+        foreach (var line in lines)
+        {
+            if (lines.Length > 0)
+            {
+                if (line.StartsWith("R"))
+                {
+                    clicks = GetAmountOfClicks(line);
+                    int numHun = clicks / 100 > 0 ? clicks / 100 : 0;
+                    dial += clicks < 100 ? clicks : clicks - (numHun * 100);
+                }
+                else if (line.StartsWith("L"))
+                {
+                    clicks = GetAmountOfClicks(line);
+                    int numHun = clicks / 100 > 0 ? clicks / 100 : 0;
+                    dial -= clicks < 100 ? clicks : clicks - (numHun * 100);
+                }
+
+                if (dial > 99)
+                {
+                    dial -= 100;
+                }
+                else if (dial < 0)
+                {
+                    dial += 100;
+                }
+
+                if (dial == 0)
+                {
+                    numOfZeros++;
+                }
+            }
+        }
+        Console.WriteLine($"Number of Zeros: {numOfZeros}");
+    }
+
+    public static int GetAmountOfClicks(string instruction)
+    {
+        return int.Parse(instruction.Substring(1)); 
+    }
+}
