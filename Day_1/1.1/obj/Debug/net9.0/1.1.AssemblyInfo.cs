59cce59ce54afded60c62a7eaada59746a342450
Aaron <asrose274@gmail.com>
2025-12-02 19:47:08 -0700

    day 2 part 1

--- Day_1/1.1/obj/Debug/net9.0/1.1.AssemblyInfo.cs
+++ Day_1/1.1/obj/Debug/net9.0/1.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b90065dc7af4d3f4dbf3a5dc463e8fb94ab6ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c54880fa70c9144706b53b429bff5f92b93d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day_2/2.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\rosea\\Repos\\AdventOfCode2025\\Day_2\\input.txt";
+
+        var lines = File.ReadAllLines(filePath);
+        string[] ranges = lines[0].Split(',');
+        long minimum;
+        long maximum;
+        long currentId;
+        long total = 0;
+
+        foreach (var range in ranges)
+        {
+            minimum = long.Parse(range.Split('-')[0]);
+            maximum = long.Parse(range.Split('-')[1]);
+            currentId = minimum;
+            while (currentId <= maximum)
+            {
+                if (validateId(currentId))
+                {
+                    total += currentId;
+                }
+                currentId++;
+            }
+        }
+
+        Console.WriteLine(total);
+    }
+
+    public static bool validateId(long id)
+    {
+        var digits = id.ToString().Length;
+        // If odd number of digits, it's valid
+        if (digits % 2 != 0)
+        {
+            return false;
+        }
+        
+        var firstHalf = id.ToString().Substring(0, digits / 2);
+        var secondHalf = id.ToString().Substring(digits / 2, digits / 2);
+
+        // If halves match, it's invalid (should be added to total)
+        if(firstHalf == secondHalf)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
+
